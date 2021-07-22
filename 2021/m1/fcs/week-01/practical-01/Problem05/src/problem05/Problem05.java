9c7a68f4f4b9b79d9664978b64310f60d2092cdb
a1845302 <a1845302@21ef3394-e2ea-4a73-a53d-106f8a3b2273>
2021-07-22 05:10:53 +0000

    Part 01 - Basic Programming - Problem 03
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1845302@4 21ef3394-e2ea-4a73-a53d-106f8a3b2273

---
+++ 2021/m1/fcs/week-01/practical-01/Problem05/src/problem05/Problem05.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package problem05;
+
+/**
+ * Problem05 - Multiplying and Printing
+ * @author haohongxing
+ */
+public class Problem05 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Integer number_a = 2;
+        Integer number_b = 3;
+        Integer number_c = number_a * number_b;
+
+        System.out.println("The multiplication of " + number_a + " and "
+                + number_b + " is " + number_c);
+    }
+    
+}
