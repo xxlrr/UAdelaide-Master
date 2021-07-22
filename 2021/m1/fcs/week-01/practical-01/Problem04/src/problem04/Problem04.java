3db96ff376538c5b437269ade8766d1e48de1119
a1845302 <a1845302@21ef3394-e2ea-4a73-a53d-106f8a3b2273>
2021-07-22 05:05:14 +0000

    Part 01 - Basic Programming - Problem 02
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1845302@3 21ef3394-e2ea-4a73-a53d-106f8a3b2273

---
+++ 2021/m1/fcs/week-01/practical-01/Problem04/src/problem04/Problem04.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package problem04;
+
+/**
+ * Problem04: Summing and Printing
+ * @author haohongxing
+ */
+public class Problem04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Integer number_a = 1;
+        Integer number_b = 2;
+        Integer number_c = number_a + number_b;
+
+        System.out.println("the sum of " + number_a + " and "
+                + number_b + " is " + number_c);
+    }
+
+}
