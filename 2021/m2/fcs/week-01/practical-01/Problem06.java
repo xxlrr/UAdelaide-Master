99993c14463323d99397cde50272c2882aa0549b
a1845302 <a1845302@21ef3394-e2ea-4a73-a53d-106f8a3b2273>
2021-07-22 08:01:25 +0000

    Adjust the directory
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1845302@7 21ef3394-e2ea-4a73-a53d-106f8a3b2273

---
+++ 2021/m2/fcs/week-01/practical-01/Problem06.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package problem06;
+
+/**
+ * @author haohongxing
+ * 
+ * Problem 06 - Division and Printing
+ */
+public class Problem06 {
+
+    public static void main(String[] args) {
+        int number_a = 3;
+        int number_b = 4;
+        float number_c = (float)number_a / number_b;
+
+        System.out.println("The multiplication of " + number_a + " and "
+                + number_b + " is " + number_c);
+    }
+}
---
+++ 2021/m2/fcs/week-01/practical-01/Problem09.java
@@ -0,0 +1,40 @@
+package problem09;
+
+import java.util.Scanner;
+
+/**
+ * @author haohongxing
+ * 
+ * Problem09: ask the dog for some information
+ */
+public class Problem09 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // prompt the dog
+        System.out.println("Hi dog, please enter the following information!");
+        
+        // a String value
+        System.out.println("Enter your name: ");
+        String name = scanner.next();
+        
+        // an int value
+        System.out.println("Enter your age: ");
+        int age = scanner.nextInt();
+        
+        // a String value
+        System.out.println("Enter your favorite food: ");
+        String food = scanner.next();
+        
+        // a String value
+        System.out.println("Enter your favorite toy: ");
+        String mathsGrade = scanner.next();
+        
+        // a String value
+        System.out.println("Enter your owner's name: ");
+        String englishGrade = scanner.next();
+
+    }
+    
+}
