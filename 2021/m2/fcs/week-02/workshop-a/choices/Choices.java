e3d64070a8e2de164b3062ca9d8c495c07bae564
a1845302 <a1845302@21ef3394-e2ea-4a73-a53d-106f8a3b2273>
2021-07-26 06:13:22 +0000

    commit workshop-a/choices assignment
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1845302@10 21ef3394-e2ea-4a73-a53d-106f8a3b2273

--- 2021/m2/fcs/week-02/workshop-a/choices/Choices.java
+++ 2021/m2/fcs/week-02/workshop-a/choices/Choices.java
@@ -4,18 +4,21 @@ import java.util.Scanner;
 
 public class Choices {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
         System.out.print("Please input an integer value: ");
         int value = scanner.nextInt();
-        if(value > 0 ){
-            System.out.println("Your value is positive value!");
-        }else{
-            System.out.println("Your value is negative value!");
+        if (value > 0) {
+            System.out.println("Your value is positive.");
         }
+        else if(value < 0){
+            System.out.println("Your value is negative.");
+        }
+        else {
+            System.out.println("Your value is zero.");
+        }
+        
         scanner.close();
     }
     
