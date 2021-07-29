05a1c92cd051d3a595ed33d8154fad826e52947f
a1845302 <a1845302@21ef3394-e2ea-4a73-a53d-106f8a3b2273>
2021-07-29 05:03:07 +0000

    add simpracexam-01/problem02
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1845302@12 21ef3394-e2ea-4a73-a53d-106f8a3b2273

---
+++ 2021/m2/fcs/week-03/simpracexam-01/problem02/Problem02.java
@@ -0,0 +1,22 @@
+//==================================
+// Foundations of Computer Science
+// Student: Hongxing Hao
+// id: a1845302
+// Semester: m2
+// Year: 2021
+// Practical Exam Number: 2
+//===================================
+package problem02;
+
+public class Problem02 {
+
+    // prints out the phrase "The sum of 3 and 8 is 11"
+    public static void main(String[] args) {
+        int number_a = 3;
+        int number_b = 8;
+        int number_c = 3 + 8;
+        
+        System.out.println("The sum of " + number_a + " and " + number_b + " is " + number_c);
+    }
+    
+}
