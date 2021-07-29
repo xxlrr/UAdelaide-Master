d3a1fb96ec801d8615f6190f799e2d397ebbf71f
a1845302 <a1845302@21ef3394-e2ea-4a73-a53d-106f8a3b2273>
2021-07-29 05:00:45 +0000

    add simpracexam-01/problem01
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1845302@11 21ef3394-e2ea-4a73-a53d-106f8a3b2273

---
+++ 2021/m2/fcs/week-03/simpracexam-01/problem01/Problem01.java
@@ -0,0 +1,18 @@
+//==================================
+// Foundations of Computer Science
+// Student: Hongxing Hao
+// id: a1845302
+// Semester: m2
+// Year: 2021
+// Practical Exam Number: 2
+//===================================
+package problem01;
+
+public class Problem01 {
+
+    public static void main(String[] args) {
+        // prints on the screen the sentence: "Hello World!".
+        System.out.println("Hello world! ");
+    }
+    
+}
