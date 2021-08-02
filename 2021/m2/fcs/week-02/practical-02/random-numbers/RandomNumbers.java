f6b5827381e7d7e8a458ccc8c1b591b79bee5c42
a1845302 <a1845302@21ef3394-e2ea-4a73-a53d-106f8a3b2273>
2021-08-02 06:47:08 +0000

    Sorry, I submitted the wrong path last week
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1845302@18 21ef3394-e2ea-4a73-a53d-106f8a3b2273

---
+++ 2021/m2/fcs/week-02/practical-02/random-numbers/RandomNumbers.java
@@ -0,0 +1,31 @@
+//==================================
+// Foundations of Computer Science
+// Student: Hongxing Hao
+// id: a1845302
+// Semester: m2
+// Year: 2021
+// Practical Number: Random Numbers
+//===================================
+package random.numbers;
+
+import java.util.Random;
+
+public class RandomNumbers {
+
+    public static void main(String[] args) {
+        // create a random generator
+        Random ran = new Random();
+        
+        // sum over 10 random numbers
+        int sum = 0;
+        for(int i=0; i<10;i++) {
+            sum += ran.nextInt(100);
+        }
+        // calculate the average number
+        float mean = sum / 10.0f;
+        
+        // echo result
+        System.out.println("The sum is " + sum + ", The mean is " + mean + ".");
+    }
+    
+}
---
+++ 2021/m2/fcs/week-02/practical-02/repetition/Problem03.java
@@ -0,0 +1,36 @@
+//==================================
+// Foundations of Computer Science
+// Student: Hongxing Hao
+// id: a1845302
+// Semester: m2
+// Year: 2021
+// Practical Number: Problem03
+//===================================
+package problem03;
+
+import java.util.Random;
+
+public class Problem03 {
+
+    public static void main(String[] args) {
+        // create a rando generator
+        Random ran = new Random();
+        
+        // generate 100 random integer
+        for(int i=0; i<100; i++) {
+            int number = ran.nextInt(100);
+            // check whether it is greater than 10 or less than 10 and print
+            if(number > 10) {
+                System.out.println("the random number is " + number + ", it is more than 10.");
+            }
+            else if(number < 10) {
+                System.out.println("the random number is " + number + ", it is less than 10.");
+            }
+            else {
+                System.out.println("the random number is " + number + ", it is equal to 10.");
+            }
+        }
+        
+    }
+    
+}
