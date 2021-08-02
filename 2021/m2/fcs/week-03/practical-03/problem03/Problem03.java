970f3e227f4ef0404b14ac6a3420961e7177ea82
a1845302 <a1845302@21ef3394-e2ea-4a73-a53d-106f8a3b2273>
2021-08-02 08:11:45 +0000

    add practical-03/problem03, "Comparison"
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1845302@22 21ef3394-e2ea-4a73-a53d-106f8a3b2273

---
+++ 2021/m2/fcs/week-03/practical-03/problem03/Problem03.java
@@ -0,0 +1,44 @@
+//==================================
+// Foundations of Computer Science
+// Student: Hongxing Hao
+// id: a1845302
+// Semester: m2
+// Year: 2021
+// Practical Number: Problem03
+//===================================
+package problem03;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Problem03 {
+
+    public static void main(String[] args) {
+        
+        try{
+            // create a scanner
+            Scanner scanner = new Scanner(System.in);
+            // prompt and receive a number
+            System.out.print("Please, insert a number: ");
+            float number1 = scanner.nextFloat();
+
+            // prompt and receive a number again
+            System.out.print("Please, insert a number again: ");
+            float number2 = scanner.nextFloat();
+
+            // get the bigger number and print
+            float maxNumber = number1 > number2 ? number1 : number2;
+            System.out.println("The bigger number is " + maxNumber);
+
+            // close the scanner
+            scanner.close();
+        }
+        // handle wrong inputs
+        catch(InputMismatchException e){
+            System.out.println("[ERROR]: your input does not match,"
+                    + " please enter an integer!");
+        }
+        
+    }
+    
+}
