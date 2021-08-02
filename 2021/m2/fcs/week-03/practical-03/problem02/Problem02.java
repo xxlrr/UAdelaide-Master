02b613b5404a1e27ffa251537686ec414f195cff
a1845302 <a1845302@21ef3394-e2ea-4a73-a53d-106f8a3b2273>
2021-08-02 07:43:32 +0000

    add practical-03/problem02, "Prime Number"
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1845302@21 21ef3394-e2ea-4a73-a53d-106f8a3b2273

---
+++ 2021/m2/fcs/week-03/practical-03/problem02/Problem02.java
@@ -0,0 +1,49 @@
+//==================================
+// Foundations of Computer Science
+// Student: Hongxing Hao
+// id: a1845302
+// Semester: m2
+// Year: 2021
+// Practical Number: Problem02
+//===================================
+package problem02;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+public class Problem02 {
+
+    public static void main(String[] args) {
+        try{
+            // prompt and accept an integer from the user
+            System.out.print("Please input a integer: ");
+            Scanner scanner = new Scanner(System.in);
+            int number = scanner.nextInt();
+            scanner.close();
+            
+            // check whether the number is prime or not
+            boolean isPrime = true;
+            for(int i=2; i<number; i++){
+                if(number%i == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            
+            // print the result
+            if(isPrime){
+                 System.out.println("Your number is prime");
+            }
+            else{
+                 System.out.println("Your number is not prime");
+            }
+        }
+        // handle wrong inputs
+        catch(InputMismatchException e){
+            System.out.println("[ERROR]: your input does not match,"
+                    + " please enter an integer!");
+        }
+    }
+    
+}
