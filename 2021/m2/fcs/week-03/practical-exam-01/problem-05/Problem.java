cf0287abee8896a8bd3d87f51a517d201ac17d74
a1845302 <a1845302@21ef3394-e2ea-4a73-a53d-106f8a3b2273>
2021-08-05 05:07:20 +0000

    add practical-exam-01/problem-05
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1845302@29 21ef3394-e2ea-4a73-a53d-106f8a3b2273

---
+++ 2021/m2/fcs/week-03/practical-exam-01/problem-05/Problem.java
@@ -0,0 +1,34 @@
+//==================================
+// Foundations of Computer Science
+// Student: Hongxing Hao
+// id: a1845302
+// Semester: m2
+// Year: 2021
+// Practical Exam Number: Problem05
+//===================================
+package problem05;
+
+
+public class Problem{
+    
+    public static void main(String [] args){
+        System.out.println("This is the last line of the code!");
+        int ths = 25;
+        for(int i = 0; i < 10; i++){
+            for(int j = i; j < 10; j++){ 
+                for (int k = j; k < 10; k ++){
+                    int value = i * j * k;
+                    if(value % ths == 0 ){
+                        if(value != 0){ 
+                            System.out.print("i: " + i);
+                            System.out.print(" j: " + j);
+                            System.out.println(" k: " + k + " - value " + value);
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println("This is the first line of the code!");
+    }
+    
+}
