daa1d5bc1154715c30d82359a5b83c80264b16c3
a1845302 <a1845302@21ef3394-e2ea-4a73-a53d-106f8a3b2273>
2021-08-05 06:19:00 +0000

    update practical-03/problem01
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1845302@32 21ef3394-e2ea-4a73-a53d-106f8a3b2273

--- 2021/m2/fcs/week-03/practical-03/problem01/Problem01.java
+++ 2021/m2/fcs/week-03/practical-03/problem01/Problem01.java
@@ -14,17 +14,24 @@ public class Problem01 {
         // create a new BaseOperations object
         BaseOperations baseOperations = new BaseOperations();  
         
-        // print a integer array
+        // handle a integer array
         int[] intArray = {1, 2, 3, 4, 5};
         baseOperations.printArray(intArray);
+        baseOperations.reverseArray(intArray);
+        int[] newIntArray = baseOperations.reverseArray(intArray);
+        baseOperations.printArray(newIntArray);
         
-        // print a string array
+        // handle a string array
         String[] strArray = {"ABC", "DEF", "GHI", "JKL", "MNO"};
         baseOperations.printArray(strArray);
+        String[] newStrArray = baseOperations.reverseArray(strArray);
+        baseOperations.printArray(newStrArray);
         
-        // print a float array
+        // handle a float array
         float[] floatArray = {1.1f, 2.2f, 3.3f, 4.4f, 5.5f};
         baseOperations.printArray(floatArray);
+        float[] newFloatArray = baseOperations.reverseArray(floatArray);
+        baseOperations.printArray(newFloatArray);
     }
 }
 
@@ -32,22 +39,65 @@ class BaseOperations {
     
     // print a integer array
     void printArray(int[] array) {
+        System.out.print("[");
         for(int i=0; i<array.length; i++) {
-            System.out.println("the array[" + i + "]: " + array[i]);
+            System.out.print(array[i]);
+            if(i != array.length-1){
+                System.out.print(", ");
+            }
         }
+        System.out.println("]");
+
     }
     
     // print a string array
     void printArray(String[] array) {
+        System.out.print("[");
         for(int i=0; i<array.length; i++) {
-            System.out.println("the array[" + i + "]: " + array[i]);
+            System.out.print(array[i]);
+            if(i != array.length-1){
+                System.out.print(", ");
+            }
         }
+        System.out.println("]");
     }
     
     // print a float array
     void printArray(float[] array) {
+        System.out.print("[");
         for(int i=0; i<array.length; i++) {
-            System.out.println("the array[" + i + "]: " + array[i]);
+            System.out.print(array[i]);
+            if(i != array.length-1){
+                System.out.print(", ");
+            }
         }
+        System.out.println("]");
+    }
+    
+    // reverse a int array
+    int[] reverseArray(int[] array) {
+        int[] newArray = new int[array.length];
+        for(int i=0; i<newArray.length; i++) {
+            newArray[i] = array[array.length-1-i];
+        }
+        return newArray;
+    }
+    
+    // reverse a string array
+    String[] reverseArray(String[] array) {
+        String[] newArray = new String[array.length];
+        for(int i=0; i<newArray.length; i++) {
+            newArray[i] = array[array.length-1-i];
+        }
+        return newArray;
+    }
+    
+    // reverse a float array
+    float[] reverseArray(float[] array) {
+        float[] newArray = new float[array.length];
+        for(int i=0; i<newArray.length; i++) {
+            newArray[i] = array[array.length-1-i];
+        }
+        return newArray;
     }
 }
