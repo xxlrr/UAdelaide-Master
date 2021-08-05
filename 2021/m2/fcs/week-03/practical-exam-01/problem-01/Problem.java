3461d0abec35436439822eb212b2d718ca1fd2e9
a1845302 <a1845302@21ef3394-e2ea-4a73-a53d-106f8a3b2273>
2021-08-05 04:01:28 +0000

    add practical-exam-01/problem-01
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1845302@25 21ef3394-e2ea-4a73-a53d-106f8a3b2273

---
+++ 2021/m2/fcs/week-03/practical-exam-01/problem-01/Problem.java
@@ -0,0 +1,18 @@
+//==================================
+// Foundations of Computer Science
+// Student: Hongxing Hao
+// id: a1845302
+// Semester: m2
+// Year: 2021
+// Practical Exam Number: Problem01
+//===================================
+package problem01;
+
+public class Problem {
+
+    public static void main(String[] args) {
+        System.out.println("This is my first practical exam");
+        System.out.println("Wish me good luck ");
+    }
+    
+}
