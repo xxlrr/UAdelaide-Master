ae9613c9bd7b713b1d80658616b613c67c5c0ab5
a1845302 <a1845302@21ef3394-e2ea-4a73-a53d-106f8a3b2273>
2021-08-05 05:27:18 +0000

    add exception
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1845302@30 21ef3394-e2ea-4a73-a53d-106f8a3b2273

--- 2021/m2/fcs/week-03/practical-exam-01/problem-04/Problem.java
+++ 2021/m2/fcs/week-03/practical-exam-01/problem-04/Problem.java
@@ -15,20 +15,25 @@ public class Problem {
 
     // convert the input miles to kilometres
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        // asks the user for a distance in miles. 
-        System.out.print("Enter a ditinct in miles: ");
-        double miles = scanner.nextDouble();
-        
-        // if the distance is in the range [0,1000] it will be converted
-        if(0 <= miles && miles <= 1000){
-            double kilometres = 1.6093 * miles;
-            System.out.printf("The distance in kilometres is %.2f\n", kilometres);
+        try {
+            Scanner scanner = new Scanner(System.in);
+            
+            // asks the user for a distance in miles. 
+            System.out.print("Enter a ditinct in miles: ");
+            double miles = scanner.nextDouble();
+            
+            // if the distance is in the range [0,1000] it will be converted
+            if(0 <= miles && miles <= 1000){
+                double kilometres = 1.6093 * miles;
+                System.out.printf("The distance in kilometres is %.2f\n", kilometres);
+            }
+            // tell the user if the distance is not in the range
+            else {
+                System.out.println("The distance " + miles + " is out of range");
+            }
         }
-        // tell the user if the distance is not in the range
-        else {
-            System.out.println("The distance " + miles + " is out of range");
+        catch(InputMismatchException e){
+            System.out.println("[ERROR]: your input does not match, please try again!");
         }
     }
     
