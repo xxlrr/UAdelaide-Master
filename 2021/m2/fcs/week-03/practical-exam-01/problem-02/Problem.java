989aa3b3dd34444b165b208bce497266dbeeb31d
a1845302 <a1845302@21ef3394-e2ea-4a73-a53d-106f8a3b2273>
2021-08-05 04:10:04 +0000

    add practical-exam-01/problem-02
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1845302@26 21ef3394-e2ea-4a73-a53d-106f8a3b2273

---
+++ 2021/m2/fcs/week-03/practical-exam-01/problem-02/Problem.java
@@ -0,0 +1,25 @@
+//==================================
+// Foundations of Computer Science
+// Student: Hongxing Hao
+// id: a1845302
+// Semester: m2
+// Year: 2021
+// Practical Exam Number: Problem02
+//===================================
+package problem02;
+
+public class Problem {
+
+    // on the screen print [101, 100, 99, 98, 97, 96, 95, 94, 93, 92, 91, 90, 89]
+    public static void main(String[] args) {
+        System.out.print("[");
+        for(int i=101; i>=89; i--) {
+            System.out.print(i);
+            if(i != 89){
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+    }
+    
+}
