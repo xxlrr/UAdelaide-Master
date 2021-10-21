0ad79bf0b709bd627766310cc702088cec27d2ea
a1845302 <a1845302@21ef3394-e2ea-4a73-a53d-106f8a3b2273>
2021-10-21 04:11:54 +0000

    delete public
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1845302@94 21ef3394-e2ea-4a73-a53d-106f8a3b2273

--- 2021/m2/fcs/week-13/final-exam/problem-03/InsertionSort.java
+++ 2021/m2/fcs/week-13/final-exam/problem-03/InsertionSort.java
@@ -11,7 +11,7 @@ public class InsertionSort {
     
     // take a sequence of integer numbers as input
     // and return a sorted array in ascending order.
-    public int[] sort(int[] array) {
+    int[] sort(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
             for (int j = i + 1; j > 0; j--) {
                 if (array[j] < array[j - 1]) {
