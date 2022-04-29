e4a12ae7b06465a1b08daf075edaca10522d4b53
a1845302 <a1845302@21ef3394-e2ea-4a73-a53d-106f8a3b2273>
2022-04-29 03:55:50 +0000

    delete useless code
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1845302@139 21ef3394-e2ea-4a73-a53d-106f8a3b2273

--- 2022/s1/cna/AlternatingBit/altbit.h
+++ 2022/s1/cna/AlternatingBit/altbit.h
@@ -4,7 +4,6 @@ extern void A_input(struct pkt);
 extern void B_input(struct pkt);
 extern void A_output(struct msg);
 extern void A_timerinterrupt(void);
-extern int A_isNewAck(struct pkt);
 
 /* included for extension to bidirectional communication */
 #define BIDIRECTIONAL 0       /*  0 = A->B  1 =  A<->B */
