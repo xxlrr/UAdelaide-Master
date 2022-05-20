575ccb04fccef8d369e7b6c13130bbdc78f8cafd
a1845302 <a1845302@21ef3394-e2ea-4a73-a53d-106f8a3b2273>
2022-05-20 04:08:59 +0000

    Implement all COMP operations
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1845302@179 21ef3394-e2ea-4a73-a53d-106f8a3b2273

--- 2022/m1/cs/assignment2/emulator.cpp
+++ 2022/m1/cs/assignment2/emulator.cpp
@@ -45,114 +45,159 @@ string disassemble_instruction(uint16_t instruction)
 }
 
 // emulate the COMP part of a cpu instruction.
-static bool emulate_comp(uint16_t instruction, int16_t* result)
+static int16_t emulate_comp(uint16_t instruction)
 {
     uint16_t iComp = (instruction & 0x1FC0) >> 6;        // 0x1FC0 == 0B 0001 1111 1100 0000
 
     switch (iComp)
     {
-    case 0B0101010:        //   "0"
-        *result = 0;
-        break;
-    case 0B0111111:        //   "1"
-        *result = 1;
-        break;
-    case 0B0111010:        //  "-1"
-        *result = -1;
-        break;
+    case 0B0000000:    return read_D() & read_A();
+    case 0B0000001:    return ~read_D() | ~read_A();
+    case 0B0000010:    return read_D() + read_A();
+    case 0B0000011:    return -read_D() - read_A() - 1;
+    case 0B0000100:    return read_D() & ~read_A();
+    case 0B0000101:    return ~read_D() | read_A();
+    case 0B0000110:    return read_D() - read_A() - 1;
+    case 0B0000111:    return read_A() - read_D();
 
-    case 0B0110000:        //   "A"
-        *result = read_A();
-        break;
-    case 0B0110011:        //  "-A"
-        *result = -read_A();
-        break;
-    case 0B0110001:        //  "!A"
-        *result = ~read_A();
-        break;
-    case 0B0110111:        // "A+1"
-        *result = read_A() + 1;
-        break;
-    case 0B0110010:        // "A-1"
-        *result = read_A() - 1;
-        break;
+    case 0B0001000:    return false;
+    case 0B0001001:    return true;
+    case 0B0001010:    return read_D();
+    case 0B0001011:    return -read_D() - 1;
+    case 0B0001100:    return read_D();
+    case 0B0001101:    return ~read_D();
+    case 0B0001110:    return read_D() - 1;
+    case 0B0001111:    return -read_D();
 
-    case 0B0001100:        //   "D"
-        *result = read_D();
-        break;
-    case 0B0001111:        //  "-D"
-        *result = -read_D();
-        break;
-    case 0B0001101:        //  "!D"
-        *result = ~read_D();
-        break;
-    case 0B0011111:        // "D+1"
-        *result = read_D() + 1;
-        break;
-    case 0B0001110:        // "D-1"
-        *result = read_D() - 1;
-        break;
+    case 0B0010000:    return ~read_D() & read_A();
+    case 0B0010001:    return read_D() | ~read_A();
+    case 0B0010010:    return read_A() - read_D() - 1;
+    case 0B0010011:    return read_D() - read_A();
+    case 0B0010100:    return ~read_D() & ~read_A();
+    case 0B0010101:    return read_D() | read_A();
+    case 0B0010110:    return -read_D() - read_A() - 2;
+    case 0B0010111:    return read_D() + read_A() + 1;
 
-    case 0B1110000:        //   "M"
-        *result = read_RAM(read_A());
-        break;
-    case 0B1110011:        //  "-M"
-        *result = -read_RAM(read_A());
-        break;
-    case 0B1110001:        //  "!M"
-        *result = ~read_RAM(read_A());
-        break;
-    case 0B1110111:        // "M+1"
-        *result = read_RAM(read_A()) + 1;
-        break;
-    case 0B1110010:        // "M-1"
-        *result = read_RAM(read_A()) - 1;
-        break;
+    case 0B0011000:    return false;
+    case 0B0011001:    return true;
+    case 0B0011010:    return -read_D() - 1;
+    case 0B0011011:    return read_D();
+    case 0B0011100:    return ~read_D();
+    case 0B0011101:    return read_D();
+    case 0B0011110:    return -read_D() - 2;
+    case 0B0011111:    return read_D() + 1;
 
-    case 0B0000010:        // "D+A"
-        *result = read_D() + read_A();
-        break;
-    case 0B0010011:        // "D-A"
-        *result = read_D() - read_A();
-        break;
-    case 0B0000111:        // "A-D"
-        *result = read_A() - read_D();
-        break;
-    case 0B0000000:        // "D&A"
-        *result = read_D() & read_A();
-        break;
-    case 0B0010101:        // "D|A"
-        *result = read_D() | read_A();
-        break;
+    case 0B0100000:    return false;
+    case 0B0100001:    return true;
+    case 0B0100010:    return read_A();
+    case 0B0100011:    return -read_A() - 1;
+    case 0B0100100:    return false;
+    case 0B0100101:    return true;
+    case 0B0100110:    return -read_A() - 1;
+    case 0B0100111:    return read_A();
 
-    case 0B1000010:        // "D+M"
-        *result = read_D() + read_RAM(read_A());
-        break;
-    case 0B1010011:        // "D-M"
-        *result = read_D() - read_RAM(read_A());
-        break;
-    case 0B1000111:        // "M-D"
-        *result = read_RAM(read_A()) - read_D();
-        break;
-    case 0B1000000:        // "D&M"
-        *result = read_D() & read_RAM(read_A());
-        break;
-    case 0B1010101:        // "D|M"
-        *result = read_D() | read_RAM(read_A());
-        break;
+    case 0B0101000:    return false;
+    case 0B0101001:    return true;
+    case 0B0101010:    return 0;
+    case 0B0101011:    return -1;
+    case 0B0101100:    return false;
+    case 0B0101101:    return true;
+    case 0B0101110:    return -1;
+    case 0B0101111:    return 0;
 
-    case 0B0001010:        // .D. ???? illegel instruction? I don't why do this?
-        *result = read_D();
-        break;
-    case 0B1111111:        // .1. ???? illegel instruction? I don't why do this?
-        *result = 1;
-        break;
+    case 0B0110000:    return read_A();
+    case 0B0110001:    return ~read_A();
+    case 0B0110010:    return read_A() - 1;
+    case 0B0110011:    return -read_A();
+    case 0B0110100:    return ~read_A();
+    case 0B0110101:    return read_A();
+    case 0B0110110:    return -read_A() - 2;
+    case 0B0110111:    return read_A() + 1;
 
-    default:
-        return false;        // illegel instruction, do nothing.
+    case 0B0111000:    return false;
+    case 0B0111001:    return true;
+    case 0B0111010:    return -1;
+    case 0B0111011:    return 0;
+    case 0B0111100:    return true;
+    case 0B0111101:    return false;
+    case 0B0111110:    return -2;
+    case 0B0111111:    return 1;
+
+    case 0B1000000:    return read_D() & read_RAM(read_A());
+    case 0B1000001:    return ~read_D() | ~read_RAM(read_A());
+    case 0B1000010:    return read_D() + read_RAM(read_A());
+    case 0B1000011:    return -read_D() - read_RAM(read_A()) - 1;
+    case 0B1000100:    return read_D() & ~read_RAM(read_A());
+    case 0B1000101:    return ~read_D() | read_RAM(read_A());
+    case 0B1000110:    return read_D() - read_RAM(read_A()) - 1;
+    case 0B1000111:    return read_RAM(read_A()) - read_D();
+
+    case 0B1001000:    return false;
+    case 0B1001001:    return true;
+    case 0B1001010:    return read_D();
+    case 0B1001011:    return -read_D() - 1;
+    case 0B1001100:    return read_D();
+    case 0B1001101:    return ~read_D();
+    case 0B1001110:    return read_D() - 1;
+    case 0B1001111:    return -read_D();
+
+    case 0B1010000:    return ~read_D() & read_RAM(read_A());
+    case 0B1010001:    return read_D() | ~read_RAM(read_A());
+    case 0B1010010:    return read_RAM(read_A()) - read_D() - 1;
+    case 0B1010011:    return read_D() - read_RAM(read_A());
+    case 0B1010100:    return ~read_D() & ~read_RAM(read_A());
+    case 0B1010101:    return read_D() | read_RAM(read_A());
+    case 0B1010110:    return -read_D() - read_RAM(read_A()) - 2;
+    case 0B1010111:    return read_D() + read_RAM(read_A()) + 1;
+
+    case 0B1011000:    return false;
+    case 0B1011001:    return true;
+    case 0B1011010:    return -read_D() - 1;
+    case 0B1011011:    return read_D();
+    case 0B1011100:    return ~read_D();
+    case 0B1011101:    return read_D();
+    case 0B1011110:    return -read_D() - 2;
+    case 0B1011111:    return read_D() + 1;
+
+    case 0B1100000:    return false;
+    case 0B1100001:    return true;
+    case 0B1100010:    return read_RAM(read_A());
+    case 0B1100011:    return -read_RAM(read_A()) - 1;
+    case 0B1100100:    return false;
+    case 0B1100101:    return true;
+    case 0B1100110:    return -read_RAM(read_A()) - 1;
+    case 0B1100111:    return read_RAM(read_A());
+
+    case 0B1101000:    return false;
+    case 0B1101001:    return true;
+    case 0B1101010:    return 0;
+    case 0B1101011:    return -1;
+    case 0B1101100:    return false;
+    case 0B1101101:    return true;
+    case 0B1101110:    return -1;
+    case 0B1101111:    return 0;
+
+    case 0B1110000:    return read_RAM(read_A());
+    case 0B1110001:    return ~read_RAM(read_A());
+    case 0B1110010:    return read_RAM(read_A()) - 1;
+    case 0B1110011:    return -read_RAM(read_A());
+    case 0B1110100:    return ~read_RAM(read_A());
+    case 0B1110101:    return read_RAM(read_A());
+    case 0B1110110:    return -read_RAM(read_A()) - 2;
+    case 0B1110111:    return read_RAM(read_A()) + 1;
+
+    case 0B1111000:    return false;
+    case 0B1111001:    return true;
+    case 0B1111010:    return -1;
+    case 0B1111011:    return 0;
+    case 0B1111100:    return true;
+    case 0B1111101:    return false;
+    case 0B1111110:    return -2;
+    case 0B1111111:    return 1;
     }
-
-    return true;
+    
+    // error do nothing.
+    return instruction;
 }
 
 // emulate the DEST part of a cpu instruction.
@@ -255,9 +300,8 @@ static void emulate_instruction()
     // C instruction
     else if ((instruction & 0xE000) == 0xE000)
     {
-        int16_t value;
-        if (emulate_comp(instruction, &value))
-            emulate_dest(instruction, value);
+        int16_t value = emulate_comp(instruction);
+        emulate_dest(instruction, value);
         emulate_jump(instruction, value);
     }
 
