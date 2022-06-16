ae08de442985ea7708152072e59d17cbc8bf0626
a1845302 <a1845302@21ef3394-e2ea-4a73-a53d-106f8a3b2273>
2022-06-16 03:48:34 +0000

    implemented the main control function containing basic functions and optimized the Node class
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1845302@229 21ef3394-e2ea-4a73-a53d-106f8a3b2273

--- 2022/s1/cna/routing/node.py
+++ 2022/s1/cna/routing/node.py
@@ -1,5 +1,6 @@
 class Node:
-    
+    """A router node."""
+
     def __init__(self, name):
         self._name = name
         self._neighbors = {}        # {neighbor: cost}
@@ -7,8 +8,8 @@ class Node:
         self._routing_table = {}    # {destination: (next_hop, cost: cost)}
 
     def set_neighbor(self, neighbor, cost):
-        # -1 means that the link is down
         if cost == -1:
+            # -1 means that the link is down
             del self._neighbors[neighbor]
         else:
             self._neighbors[neighbor] = cost
@@ -22,15 +23,15 @@ class Node:
 
     def link_states_to_string(self):
         str = f"{self._name} LSDB:\n"
-        for nodes, cost in sorted(self._link_states.items()):
-            str += f"{nodes[0]},{nodes[1]},{cost}\n"
+        for (node1, node2), cost in sorted(self._link_states.items()):
+            str += f"{node1},{node2},{cost}\n"
         str += "\n"
         return str
 
     def routing_table_to_string(self):
         str = f"{self._name} Routing Table:\n"
-        for dest, next in sorted(self._link_states.items()):
-            str += f"{dest},{next[0]},{next[1]}\n"
+        for dest, (next, cost) in sorted(self._link_states.items()):
+            str += f"{dest},{next},{cost}\n"
         str += "\n"
         return str
 
@@ -43,13 +44,44 @@ class Node:
         return self._name
 
 
+def main():
+    """Main control function, used to handle input, output and program flow."""
+    
+    # reading route nodes from input
+    nodes = {}
+    while True:
+        line = input()
+        if line == "LINKSTATE":
+            break
+        nodes[line] = Node(line)
+
+    # reading link states from input
+    while True:
+        line = input()
+        if line == "UPDATE":
+            break
+        fields = line.split()   # fields == (name, neighbor, cost, [router_list])
+        nodes[fields[0]].set_neighbor(fields[1], int(fields[2]))
+        if len(fields) == 4:
+            for name in fields[3].split(","):
+                print(nodes[name].detail_to_string(), end="")
+        # todo: Dijkstra()
+
+    # update link states
+    while True:
+        line = input()
+        if line == "END":
+            break
+        fields = line.split()   # fields == (name, neighbor, cost, [router_list])
+        if fields[1] not in nodes:
+            nodes[line] = Node(fields[1])
+        nodes[fields[0]].set_neighbor(fields[1], int(fields[2]))
+        if len(fields) == 4:
+            for name in fields[3].split(","):
+                print(nodes[name].detail_to_string(), end="")
+        # todo: Dijkstra()
+    
+
 # for debug
 if __name__ == "__main__":
-    node = Node("D")
-    node.set_neighbor("C", 1)
-    node.set_neighbor("B", 2)
-    node.set_neighbor("A", 3)
-    print(node.neighbors_to_string(), end="")
-    print(node.link_states_to_string(), end="")
-    print(node.routing_table_to_string(), end="")
-    print(node.detail_to_string(), end="")
+    main()
