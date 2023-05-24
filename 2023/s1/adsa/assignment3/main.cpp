e09ed9b7b308f0baf5252d739f173e8c359f3f95
a1845302 <a1845302@21ef3394-e2ea-4a73-a53d-106f8a3b2273>
2023-05-24 11:53:15 +0000

    add adsa.a3
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1845302@246 21ef3394-e2ea-4a73-a53d-106f8a3b2273

---
+++ 2023/s1/adsa/assignment3/main.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+class HashTable {
+private:
+    struct Slot {
+        std::string key;
+        enum Status { NEVER_USED, TOMBSTONE, OCCUPIED } status;
+    };
+
+    std::vector<Slot> table;
+
+public:
+    HashTable() {
+        table.resize(26);
+        for (auto& slot : table) {
+            slot.status = Slot::NEVER_USED;
+        }
+    }
+
+    char hashFunction(const std::string& key) const {
+        return key.back();
+    }
+
+    int findSlot(const std::string& key) const {
+        char hashValue = hashFunction(key);
+        int slotIndex = hashValue - 'a';
+
+        while (true) {
+            if (table[slotIndex].status == Slot::NEVER_USED)
+                break;
+            else if (table[slotIndex].status == Slot::OCCUPIED && table[slotIndex].key == key)
+                break;
+
+            // Move to the next slot
+            slotIndex = (slotIndex + 1) % 26;
+        }
+
+        return slotIndex;
+    }
+
+    void insert(const std::string& key) {
+        int slotIndex = findSlot(key);
+
+        if (table[slotIndex].status != Slot::OCCUPIED) {
+            table[slotIndex].key = key;
+            table[slotIndex].status = Slot::OCCUPIED;
+        }
+    }
+
+    void remove(const std::string& key) {
+        int slotIndex = findSlot(key);
+
+        if (table[slotIndex].status == Slot::OCCUPIED)
+            table[slotIndex].status = Slot::TOMBSTONE;
+    }
+
+    void printKeys() const {
+        for (const auto& slot : table) {
+            if (slot.status == Slot::OCCUPIED)
+                std::cout << slot.key << " ";
+        }
+        std::cout << std::endl;
+    }
+};
+
+int main() {
+    HashTable hashTable;
+
+    std::string moves;
+    std::getline(std::cin, moves);
+
+    size_t pos = 0;
+    std::string move;
+    while ((pos = moves.find(' ')) != std::string::npos) {
+        move = moves.substr(0, pos);
+
+        if (move[0] == 'A') {
+            std::string key = move.substr(1);
+            hashTable.insert(key);
+        } else if (move[0] == 'D') {
+            std::string key = move.substr(1);
+            hashTable.remove(key);
+        }
+
+        moves.erase(0, pos + 1);
+    }
+
+    if (!moves.empty()) {
+        if (moves[0] == 'A') {
+            std::string key = moves.substr(1);
+            hashTable.insert(key);
+        } else if (moves[0] == 'D') {
+            std::string key = moves.substr(1);
+            hashTable.remove(key);
+        }
+    }
+
+    hashTable.printKeys();
+
+    return 0;
+}
